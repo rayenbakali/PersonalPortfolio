a455034822268d7a765bbc5b7ca1f1aa023248db
Rayen Bakali <rayen.bakali@gmail.com>
2023-11-26 00:09:07 +0100

    updates

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -90,22 +90,10 @@ export const Projects = () => {
                         </Row>
                       </Tab.Pane>
                       <Tab.Pane eventKey="second">
-                        <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Cumque quam, quod neque provident velit, rem
-                          explicabo excepturi id illo molestiae blanditiis,
-                          eligendi dicta officiis asperiores delectus quasi
-                          inventore debitis quo.
-                        </p>
+                        <p>I will be working on the project Section Soon</p>
                       </Tab.Pane>
                       <Tab.Pane eventKey="third">
-                        <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Cumque quam, quod neque provident velit, rem
-                          explicabo excepturi id illo molestiae blanditiis,
-                          eligendi dicta officiis asperiores delectus quasi
-                          inventore debitis quo.
-                        </p>
+                        <p>Working In progress as i said :3</p>
                       </Tab.Pane>
                     </Tab.Content>
                   </Tab.Container>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -96,7 +96,7 @@ export const Skills = () => {
           </div>
         </div>
       </div>
-      <img className="background-image-left" src={colorSharp} alt="Image" />
+      <img className="background-image-left" src={colorSharp} alt="left" />
     </section>
   );
 };
