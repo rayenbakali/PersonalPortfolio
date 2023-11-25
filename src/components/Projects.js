b687b5aea7278df17a8fad975f45dbf352bcba3b
Rayen Bakali <rayen.bakali@gmail.com>
2023-11-26 00:17:06 +0100

    DNS

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -103,7 +103,11 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img
+        className="background-image-right"
+        src={colorSharp2}
+        alt="salem"
+      ></img>
     </section>
   );
 };
